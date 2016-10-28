7922dc3b0ed5e27063a62b78d17ff90d56cd957c
mazzy <mazzy@mazzy.ru>
2016-10-28 18:20:48 +0300

    v2016-10-28
    To avoid lost unsaved work the job don't execute steps 'kill other processes' and 'restore' if some known programs (SQL Management Studio, Visual Studio) use database.

--- db-reset.cmd
+++ db-reset.cmd
@@ -7,7 +7,7 @@ set exitOk=EXIT /b 0
 SET cmdName=%~n0
 SET cmdNameExt=%~n0%~x0
 SET cmdPath=%~p0
-SET cmdTitle=%cmdNameExt%: Restore databse from the snapshot. by mazzy, v2016-10-12
+SET cmdTitle=%cmdNameExt%: Restore databse from the snapshot. by mazzy, v2016-10-28
 TITLE %cmdTitle%
 
 ECHO.
@@ -15,21 +15,23 @@ ECHO %cmdTitle%
 ECHO Use parameter -h for more help. see also https://github.com/mazzy-ax/ax7db-reset
 
 set services=DynamicsAxBatch MR2012ProcessService Microsoft.Dynamics.AX.Framework.Tools.DMF.SSISHelperService.exe MSSQLServerOLAPService ReportServer
+set killProhibitIf=Microsoft SQL Server Management Studio^|Microsoft SQL Server Data Tools
 
 :ParmLoop
-CALL :isParmStr "%1" "%2" "db"              "AxDbRain"                 && shift && shift && goto :ParmLoop
-CALL :isParmStr "%1" "%2" "snapshot"        "AxDBRAINInitialDataState" && shift && shift && goto :ParmLoop
-CALL :isParmStr "%1" "%2" "servicesToStop"  "%services%"               && shift && shift && goto :ParmLoop
-CALL :isParmStr "%1" "%2" "servicesToStart" "%services%"               && shift && shift && goto :ParmLoop
-CALL :isParmStr "%1" "%2" "log"             "nul"                      && shift && shift && goto :ParmLoop
-CALL :isParmInt "%1" "%2" "timeout"         7 -1                       && shift && shift && goto :ParmLoop
-CALL :isParmInt "%1" "%2" "retry"           5  0                       && shift && shift && goto :ParmLoop
-CALL :isParmInt "%1" "%2" "waitUntilStart"  1  0 1                     && shift && shift && goto :ParmLoop
-CALL :isParmInt "%1" "%2" "pauseAtEnd"      1  0 1                     && shift && shift && goto :ParmLoop
-CALL :isParmInt "%1" "%2" "killOther"       1  0 1                     && shift && shift && goto :ParmLoop
-CALL :isParm    "%1" "?"                                               && shift          && goto :Usage    
-CALL :isParm    "%1" "h"                                               && shift          && goto :Usage    
-CALL :isParm    "%1" "help"                                            && shift          && goto :Usage    
+CALL :isParmStr "%1" "%2" "db"                 "AxDbRain"                 && shift && shift && goto :ParmLoop
+CALL :isParmStr "%1" "%2" "snapshot"           "AxDBRAINInitialDataState" && shift && shift && goto :ParmLoop
+CALL :isParmStr "%1" "%2" "servicesToStart"    "%services%"               && shift && shift && goto :ParmLoop
+CALL :isParmStr "%1" "%2" "servicesToStop"     "%services%"               && shift && shift && goto :ParmLoop
+CALL :isParmStr "%1" "%2" "log"                "nul"                      && shift && shift && goto :ParmLoop
+CALL :isParmInt "%1" "%2" "timeout"            7 -1                       && shift && shift && goto :ParmLoop
+CALL :isParmInt "%1" "%2" "retry"              5  0                       && shift && shift && goto :ParmLoop
+CALL :isParmInt "%1" "%2" "waitUntilStart"     1  0 1                     && shift && shift && goto :ParmLoop
+CALL :isParmInt "%1" "%2" "pauseAtEnd"         1  0 1                     && shift && shift && goto :ParmLoop
+CALL :isParmInt "%1" "%2" "killOther"          1  0 1                     && shift && shift && goto :ParmLoop
+CALL :isParmStr "%1" "%2" "killProhibitIf"                                && shift && shift && goto :ParmLoop
+CALL :isParm    "%1" "?"                                                  && shift          && goto :Usage    
+CALL :isParm    "%1" "h"                                                  && shift          && goto :Usage    
+CALL :isParm    "%1" "help"                                               && shift          && goto :Usage    
 if not "%1"=="" (
   CALL :Print "%1 is an unrecognized or obsolete option."
   shift
@@ -39,7 +41,7 @@ if not "%1"=="" (
 set logErr=con
 if not "%log%"=="nul" (
   set logErr=%log%
-  set logPS=^|Out-File %log% -Encoding "ASCII" -Width 1024 -Append
+  set logPS=-I- Out-File %log% -Encoding "ASCII" -Width 1024 -Append
 )
 echo %cmdTitle%>%log%
 
@@ -64,15 +66,14 @@ CALL :printTitle "stop IIS"
 iisreset /stop /status >>%log%&& CALL :PrintOk
 
 CALL :printTitle "Kill other processes that used database %db%"
-CALL :PrintOther %db%
 IF "%killOther%"=="1" (
-  CALL :KillOther %db%
+  CALL :isKillAllowed %db% && CALL :KillOther %db% && (
+    CALL :printTitle "Restore database %db% from snapshot %snapshot%"
+    CALL :RestoreDatabase %db% %snapshot%
+  )
 ) ELSE (
   CALL :PrintSkipped
 )
-
-CALL :printTitle "Restore database %db% from snapshot %snapshot%"
-CALL :RestoreDatabase %db% %snapshot%
 
 CALL :printTitle "start services: %servicesToStart%"
 IF not "%servicesToStart%"=="" (
@@ -91,8 +92,29 @@ endlocal
 goto :EOF
 
 :: ------------------------
-:PrintOther %1 - database name
-powershell "$p=invoke-sqlcmd """SELECT spid,rtrim(status) as status,hostprocess as id,rtrim(hostname) as hostname,rtrim(loginame) as login,rtrim(program_name) as program,rtrim(nt_domain)+rtrim(nt_username) as 'user' FROM MASTER..SysProcesses WHERE DBId=DB_ID('%1') AND NOT SPId=@@SPId""";if($p.Count -eq 0){'nothing to kill' %logPs%}else{$p|group status,id,hostname,user,login,program -NoElement|ft -AutoSize %logPs%;$p|where {$_.hostname -eq '%computername%' -and $_.id -ne 0}|ps|ft id, name, product, path %logPs%}"
+:isKillAllowed %1 - database name
+set cmd=
+set cmd=%cmd% $p=invoke-sqlcmd """SELECT
+set cmd=%cmd%   spid,
+set cmd=%cmd%   rtrim(status) as status,
+set cmd=%cmd%   cast(hostprocess as numeric) as id,
+set cmd=%cmd%   rtrim(hostname) as hostname,
+set cmd=%cmd%   rtrim(loginame) as login,
+set cmd=%cmd%   rtrim(program_name) as program,
+set cmd=%cmd%   rtrim(nt_domain)+rtrim(nt_username) as 'user'
+set cmd=%cmd%   FROM MASTER..SysProcesses WHERE
+set cmd=%cmd%   DBId=DB_ID('%1') AND NOT SPId=@@SPId""";
+set cmd=%cmd% if(!$p.Count)
+set cmd=%cmd% {
+set cmd=%cmd%   'nothing to kill' %logPs%;
+set cmd=%cmd%   exit 0;
+set cmd=%cmd% }
+set cmd=%cmd% $p-I-sort status,id,hostname-I-group status,id,hostname,user,login,program -NoElement-I-ft -AutoSize %logPs%;
+set cmd=%cmd% $p-I-where {$_.hostname -eq $env:COMPUTERNAME -and $_.id -ne 0}-I-ps-I-ft id, name, product, path %logPs%;
+set cmd=%cmd% $p.program -match $env:killProhibitIf-I-sort-I-group-I-ft -autosize @{Label='Programs prevent restore database. Use -h for more help.';Expression={$_.name}} %logPs%;
+set cmd=%cmd% if($?){exit 0;}else{exit 1;}
+set cmd=%cmd:-I-=^|%
+powershell "%cmd%"||%exitFail%
 %exitOk%
 
 :KillOther %1 - database name
@@ -134,7 +156,7 @@ FOR /L %%r IN (%3,-1,1) DO (
 
 
 :printTitle
-CALL :Print "----"
+CALL :Print "===="
 CALL :Print %1
 %exitOk%
 
@@ -161,7 +183,7 @@ ECHO [%Date% %Time%]: %~1 >>%log%
 
 
 :isParmInt %1 - parm name, %2 - parm value, %3 - keyword, %4 - default value, %5 - min value (optional), %6 - max value (optional)
-if not defined %~3 set %~3=%~4
+if not defined %~3 if not ""=="%~4" set %~3=%~4
 if "%~2"=="" %exitFail%
 CALL :isParm %1 %3||%exitFail%
 CALL :Validate %2 %5 %6||%exitOk%
@@ -169,7 +191,7 @@ set /a %~3=%~2
 %exitOk%
 
 :isParmStr %1 - parm name, %2 - parm value, %3 - keyword, %4 - default value
-if not defined %~3 set %~3=%~4
+if not defined %~3 if not ""=="%~4" set %~3=%~4
 if "%~2"=="" %exitFail%
 CALL :isParm %1 %3||%exitFail%
 set %~3=%~2
@@ -197,14 +219,15 @@ if %~1 GTR %~3 %exitFail%
 
 :Usage
 echo -----------------------------------------------
-echo The restore command requires no other process use the database.
-echo This job:
+echo This job restore database from snapshot:
 echo   1. stop known services and IIS
 echo   2. wait until stopped
 echo   3. kill other processes that use database
 echo   4. restore database
 echo   5. start known services and IIS
 echo   6. wait until start
+echo.
+echo To avoid lost unsaved work the job don't execute steps 'kill other processes' and 'restore' if some known programs (SQL Management Studio, Visual Studio) use database. Close relative queries, object explorers or programs.
 echo -----------------------------------------------
 echo   %cmdNameExt% [options] 
 echo.
@@ -212,9 +235,10 @@ echo Options:
 echo   -db             :database name. default=AxDBRAIN
 echo   -snapshot       :snapshot name. default=AxDBRAINInitialDataState
 echo   -servicesToStop :known service names to stop before the resotre.
-echo                    default="%services%"
 echo                    empty string "" prevent stop any services.
 echo   -killOther      :kill other processes. 0 or 1. default=1
+echo   -killProhibitIf :reqExp for match program that prohibit kill process.
+echo                    empty string "" to kill'em all.
 echo   -servicesToStart:known service names to start after the restore.
 echo                    default=same as -servicesToStop
 echo                    empty string "" prevent start.
